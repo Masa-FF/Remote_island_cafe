ca88fa6f4e771ace66a8aada012a106966f9d3df
EC2 Default User <ec2-user@ip-172-31-10-66.ap-northeast-1.compute.internal>
2020-12-20 04:31:42 +0000

    Add_favorite_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
-  resources :photos
-  resource :favorites, only: [:create, :destroy]
+  resources :photos do
+    resource :favorites, only: [:create, :destroy]
+  end
   get 'top' => 'homes#top'
   get 'menu' => 'homes#menu'
 end
